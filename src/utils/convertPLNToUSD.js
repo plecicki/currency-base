5fa93c45b110d42c6875c28e56fade81c49837fe
Piotr Lecicki <p.lecicki@gmail.com>
2025-05-08 20:39:37 +0200

    Add unit tests

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,6 +1,9 @@
 export const convertPLNToUSD = (PLN) => {
 
-  const PLNtoUSD = PLN / 3.5;
+  if (typeof PLN === 'undefined' || typeof PLN === 'string') return NaN;
+  else if (typeof PLN !== 'number') return 'Error';
+
+  const PLNtoUSD = PLN > 0 ? PLN / 3.5 : 0;
   
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
