df9d6770b63ec1c58cd9b6c2fd742339267ee826
Piotr Lecicki <p.lecicki@gmail.com>
2025-05-10 14:12:04 +0200

    Add unit tests for CurrencyForm

---
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import CurrencyForm from './CurrencyForm';
+import userEvent from '@testing-library/user-event';
+import { cleanup } from "@testing-library/react";
+
+describe('Component CurrencyForm', () => {
+  it('should render without crashing', () => {
+    render(<CurrencyForm action={() => {}} />);
+  });
+  it('should run action callback with proper data on form submit', () => {
+    const testCases = [
+      { amount: '100', from: 'PLN', to: 'USD' },
+      { amount: '20', from: 'USD', to: 'PLN' },
+      { amount: '200', from: 'PLN', to: 'USD' },
+      { amount: '345', from: 'USD', to: 'PLN' },
+    ];
+
+    for(const testObj of testCases) {
+      const action = jest.fn();
+
+      // render component
+      render(<CurrencyForm action={action} />);
+
+      // find “convert” button
+      const submitButton = screen.getByText('Convert');
+      const amountField = screen.getByTestId('amount');
+      const fromField = screen.getByTestId('select-from');
+      const toField = screen.getByTestId('select-to');
+
+      // set test values to fields
+      userEvent.type(amountField, testObj.amount);
+      userEvent.selectOptions(fromField, testObj.from);
+      userEvent.selectOptions(toField, testObj.to);
+
+      // simulate user click on "convert" button
+      userEvent.click(submitButton);
+
+      // check if action callback was called once
+      expect(action).toHaveBeenCalledTimes(1);
+      expect(action).toHaveBeenCalledWith({ amount: parseInt(testObj.amount), from: testObj.from, to: testObj.to });
+
+      //Clean
+      cleanup();
+    }
+  });
+});
